ffd7ca793dc4f6e353b812c6cdb7c2777eda3ae5
Ben Pfaff <bpfaff@vmware.com>
2021-02-17 11:02:00 -0800

    generate.sh: Tell user if they forget to set $DDLOG_HOME
    I forgot and the resulting error message wasn't very helpful.

--- nerpa_controlplane/generate.sh
+++ nerpa_controlplane/generate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if test ! -f "$DDLOG_HOME/lib/ddlog_std.dl"; then
+    echo >&2 "$0: \$DDLOG_HOME must point to the ddlog tree"
+    exit 1
+fi
+
 ddlog -i nerpa.dl &&
 (cd nerpa_ddlog && cargo build --release && cd ..) &&
 ./nerpa_ddlog/target/release/nerpa_cli < nerpa.dat
