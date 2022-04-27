915df2a7d7f9c471b6cf00b82aa778dcaa95044c
Mihai Budiu <mbudiu@vmware.com>
2022-04-26 20:40:18 -0700

    Skeleton p4c-of compiler
    Signed-off-by: Mihai Budiu <mbudiu@vmware.com>

--- ofp4/README.md
+++ ofp4/README.md
@@ -1,14 +1,39 @@
-# What is ofp4?
+# Compiling P4 to Open-Flow
 
-ofp4 provides a P4Runtime interface to Open vSwitch.  It accepts
-P4Runtime connections from a controller and connects to an Open
-vSwitch instance over OpenFlow and OVSDB.
+This directory contains a compiler that converts P4 code to Open-Flow.
+The target for this compiler is of_model.p4, which can be found in the
+`p4include` directory.
 
-ofp4 is an experimental prototype.  Currently, the translation from P4
-to OpenFlow is handwritten and has only been done for
+The compiler in fact consumes a P4 program and generates a
+Differential Datalog (DDlog) program
+(https://github.com/vmware/differential-datalog).  The DDlog program
+implements a controller called ofp4, which controls an Open-Flow
+programmable device.
+
+Currently, the translation from P4 to DDlog has been handwritten for
 `nerpa_controlplane/snvs/snvs.p4`.  You can find this handwritten
-translation in `snvs.dl`.  This translation has to be automated by
-writing a p4c backend, but that work hasn't been started yet.
+translation in `snvs.dl`.  Eventually the compiler will generate an
+equivalent form.
+
+## Compiling the p4c-of compiler
+
+To build the P4 compiler with this backend you have to perform the
+following steps:
+
+* Check out the p4c repository from the above address and make sure you can compile the code
+* Create a symbolic link in the directory p4c/extensions pointing to this directory
+* Rebuild the p4c compiler using the standard methodology.
+
+The result of the compilation should be a `p4c-of` binary.
+
+## What is ofp4?
+
+ofp4 is essentially a controller with a P4Runtime interface that
+controls Open vSwitch.  It accepts P4Runtime connections from a
+controller and connects to an Open vSwitch instance over OpenFlow and
+OVSDB.
+
+ofp4 is an experimental prototype.
 
 So far, ofp4 accepts P4Runtime connections and allows multicast groups
 and table entries to be updated, translates those into OpenFlow flows
@@ -30,7 +55,7 @@ Pass `--ofp4` to `scripts/run-nerpa.sh` to make it start up OVS and
 ofp4 instead of bmv2.  This won't pass the tests, since MAC learning
 won't work yet.
 
-# Related Work
+## Related Work
 
 The P4 OpenFlow Agent (https://github.com/p4lang/p4ofagent) provides
 an OpenFlow interface to a P4 switch, the opposite of ofp4.
---
+++ ofp4/midend.cpp
@@ -0,0 +1,109 @@
+/*
+Copyright 2022 VMware, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "frontends/common/constantFolding.h"
+#include "frontends/common/resolveReferences/resolveReferences.h"
+#include "frontends/p4/evaluator/evaluator.h"
+#include "frontends/p4/fromv1.0/v1model.h"
+#include "frontends/p4/moveDeclarations.h"
+#include "frontends/p4/simplify.h"
+#include "frontends/p4/strengthReduction.h"
+#include "frontends/p4/toP4/toP4.h"
+#include "frontends/p4/typeMap.h"
+#include "frontends/p4/unusedDeclarations.h"
+#include "midend.h"
+#include "midend/actionSynthesis.h"
+#include "midend/compileTimeOps.h"
+#include "midend/complexComparison.h"
+#include "midend/copyStructures.h"
+#include "midend/eliminateTuples.h"
+#include "midend/eliminateNewtype.h"
+#include "midend/eliminateSerEnums.h"
+#include "midend/eliminateSwitch.h"
+#include "midend/flattenHeaders.h"
+#include "midend/flattenInterfaceStructs.h"
+#include "midend/hsIndexSimplify.h"
+#include "midend/expandEmit.h"
+#include "midend/expandLookahead.h"
+#include "midend/global_copyprop.h"
+#include "midend/local_copyprop.h"
+#include "midend/midEndLast.h"
+#include "midend/nestedStructs.h"
+#include "midend/noMatch.h"
+#include "midend/predication.h"
+#include "midend/removeExits.h"
+#include "midend/removeMiss.h"
+#include "midend/simplifyKey.h"
+#include "midend/tableHit.h"
+#include "midend/removeAssertAssume.h"
+
+namespace P4OF {
+
+MidEnd::MidEnd(CompilerOptions& options) {
+    bool isv1 = options.langVersion == CompilerOptions::FrontendVersion::P4_14;
+    refMap.setIsV1(isv1);
+    auto evaluator = new P4::EvaluatorPass(&refMap, &typeMap);
+    setName("MidEnd");
+
+    addPasses({
+        options.ndebug ? new P4::RemoveAssertAssume(&refMap, &typeMap) : nullptr,
+        new P4::RemoveMiss(&refMap, &typeMap),
+        new P4::EliminateNewtype(&refMap, &typeMap),
+        new P4::EliminateSerEnums(&refMap, &typeMap),
+        new P4::SimplifyKey(&refMap, &typeMap,
+                            new P4::OrPolicy(
+                                new P4::IsValid(&refMap, &typeMap),
+                                new P4::IsLikeLeftValue())),
+        new P4::RemoveExits(&refMap, &typeMap),
+        new P4::ConstantFolding(&refMap, &typeMap),
+        new P4::ExpandLookahead(&refMap, &typeMap),
+        new P4::ExpandEmit(&refMap, &typeMap),
+        new P4::HandleNoMatch(&refMap),
+        new P4::StrengthReduction(&refMap, &typeMap),
+        new P4::EliminateTuples(&refMap, &typeMap),
+        new P4::SimplifyComparisons(&refMap, &typeMap),
+        new P4::CopyStructures(&refMap, &typeMap, false),
+        new P4::NestedStructs(&refMap, &typeMap),
+        new P4::FlattenHeaders(&refMap, &typeMap),
+        new P4::FlattenInterfaceStructs(&refMap, &typeMap),
+        new P4::Predication(&refMap),
+        new P4::MoveDeclarations(),  // more may have been introduced
+        new P4::ConstantFolding(&refMap, &typeMap),
+        new P4::GlobalCopyPropagation(&refMap, &typeMap),
+        new PassRepeated({
+            new P4::LocalCopyPropagation(&refMap, &typeMap),
+            new P4::ConstantFolding(&refMap, &typeMap),
+        }),
+        new P4::StrengthReduction(&refMap, &typeMap),
+        new P4::MoveDeclarations(),  // more may have been introduced
+        new P4::SimplifyControlFlow(&refMap, &typeMap),
+        new P4::CompileTimeOperations(),
+        new P4::TableHit(&refMap, &typeMap),
+        new P4::EliminateSwitch(&refMap, &typeMap),
+        evaluator,
+        new P4::HSIndexSimplifier(&refMap, &typeMap),
+        new P4::SynthesizeActions(&refMap, &typeMap),
+        new P4::MoveActionsToTables(&refMap, &typeMap),
+        evaluator,
+        [this, evaluator]() { toplevel = evaluator->getToplevelBlock(); },
+        new P4::MidEndLast()
+    });
+    if (options.excludeMidendPasses) {
+        removePasses(options.passesToExcludeMidend);
+    }
+}
+
+}  // namespace P4OF
