vmware-archive/nerpa

commit 9d87189347dbb5d2923d0de380b3a9ab3e82891e

Improve register allocation and sanity checks
Signed-off-by: Mihai Budiu <mbudiu@vmware.com>

ofp4/of.cpp
@@ -34,6 +34,22 @@ Constant OF_Register::mask() const {
     return Constant::GetMask(high+1) ^ Constant::GetMask(low);
 }
 
+void OF_Register::validate() const {
+    BUG_CHECK(low <= high, "low %1% > high %2", low, high);
+    BUG_CHECK(high - low <= size, "high %1% - low %2% > size %3%", high, low, size);
+    BUG_CHECK(size <= registerSize * maxBundleSize, "size %1% > max %2%",
+              size, registerSize * maxBundleSize);
+    size_t bundle = 32;
+    size_t bytes = ROUNDUP(size, 8);
+    while (bytes > 4) {
+        bytes = bytes >> 1;
+        bundle = bundle << 1;
+    }
+    BUG_CHECK(low / bundle == high / bundle,
+              "start %1% and end %2% bytes in different registers",
+              ROUNDUP(low, 8), ROUNDUP(high, 8));
+}
+
 cstring OF_Register::asDDlogString(bool inMatch) const {
     // A register is written differently depending on the position
     // in the OF statement.

ofp4/controlFlowGraph.cpp
@@ -136,7 +136,7 @@ class CFGBuilder : public Inspector {
         return false;
     }
     bool preorder(const IR::IfStatement* statement) override {
-        // We only allow expressions of the form t.apply().hit lively.
+        // We only allow expressions of the form t.apply().hit.
         // If the expression is more complex it should have been
         // simplified by prior passes.
         auto tc = P4::TableApplySolver::isHit(statement->condition, refMap, typeMap);
@@ -149,6 +149,7 @@ class CFGBuilder : public Inspector {
             // hit-miss case.
             node = cfg->makeNode(tc, statement->condition);
         } else {
+            // regular control-flow, not table-dependent
             node = cfg->makeNode(statement);
         }
 
@@ -190,6 +191,7 @@ class CFGBuilder : public Inspector {
         node->addPredecessors(live);
         auto result = new CFG::EdgeSet(new CFG::Edge(node));  // In case no label matches
         auto labels = new CFG::EdgeSet();
+        live = new CFG::EdgeSet();
         for (auto sw : statement->cases) {
             cstring label;
             if (sw->label->is<IR::DefaultExpression>()) {

ofp4/of.def
@@ -41,6 +41,7 @@ class OF_Register : OF_Expression {
     cstring toString() const;
     /// Generates a DDlog expression that returns the register at runtime.
     cstring asDDlogString(bool inMatch) const;
+    void validate() const;
 #emit
     Constant mask() const;
 #end

ofp4/resources.h
@@ -27,7 +27,7 @@ limitations under the License.
 namespace OFP4 {
 
 /// This class reprents the OF resources used by a P4 program
-class OFResources {
+class OFResources : public IHasDbPrint {
     P4::TypeMap* typeMap;
     std::map<const IR::IDeclaration*, const IR::OF_Register*> map;
     // One bit for each register byte; if 'true' the byte is allocated.
@@ -72,6 +72,11 @@ class OFResources {
         size_t bytesNeeded = ROUNDUP(width, 8);
         // Find a gap of bytesNeeded bytes
         for (size_t i = 0; i < byteMask.size() - bytesNeeded + 1; i++) {
+            // The data must fit in the current register bundle.
+            if ((i * 8) / size != (i * 8 + width - 1) / size)
+                // start and end in different registers.
+                continue;
+
             if (!byteMask.at(i)) {
                 found = true;
                 for (size_t j = i + 1; j < i + bytesNeeded; j++) {
@@ -82,7 +87,7 @@ class OFResources {
                     }
                 }
                 if (found) {
-                    LOG3("Allocating " << bytesNeeded << " at " << i);
+                    LOG3("Allocating " << bytesNeeded << " bytes at offset " << i);
                     index = i;
                     break;
                 }
@@ -104,13 +109,25 @@ class OFResources {
                                           type->is<IR::Type_Boolean>(),
                                           makeId(decl->externalName()));
         map.emplace(decl, result);
-        LOG3("Allocated " << result->toString() << " for " << decl);
+        LOG3("Allocated " << result->toString() << " for " << decl << " width " <<
+             size << std::endl << this);
         return result;
     }
 
     const IR::OF_Register* getRegister(const IR::IDeclaration* decl) const {
         auto result = ::get(map, decl);
         return result;
+    }
+
+    void dbprint(std::ostream& out) const {
+        size_t index = 0;
+        for (bool b: byteMask) {
+            out << (b ? "X" : "_");
+            index++;
+            if (!(index % 4))
+                out << " ";
+        }
+        out << std::endl;
     }
 };
 

commit af2542382a80903a87f9a065a43bfbb96e5a7037

ofp4: Fix format string in OF_Register::validate().
Signed-off-by: Ben Pfaff <bpfaff@vmware.com>
Fixes: 9d87189347db ("Improve register allocation and sanity checks")

ofp4/of.cpp
@@ -35,7 +35,7 @@ Constant OF_Register::mask() const {
 }
 
 void OF_Register::validate() const {
-    BUG_CHECK(low <= high, "low %1% > high %2", low, high);
+    BUG_CHECK(low <= high, "low %1% > high %2%", low, high);
     BUG_CHECK(high - low <= size, "high %1% - low %2% > size %3%", high, low, size);
     BUG_CHECK(size <= registerSize * maxBundleSize, "size %1% > max %2%",
               size, registerSize * maxBundleSize);